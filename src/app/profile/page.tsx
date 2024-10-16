3404d118c01db626034b118ac83b05cfa97aab9b
Felipe Rosa Peres <feliperosaperes2004@gmail.com>
2024-10-16 15:59:16 -0300

    Paginas e components tudo

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+import { Header } from "@/components/Header/Header";
+import { Layout } from "@/components/Layout/Layout";
+import UserContext from "@/context/UserContext";
+import { useVerifyLogin } from "@/helpers/useVerifyLogin";
+import { jwtDecode } from "jwt-decode";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Profile() {
+    useVerifyLogin();
+    const navigate = useNavigate();
+  
+    const { userName, setUserName } = useContext(UserContext);
+  
+    useEffect(() => {
+      const userToken = JSON.parse(sessionStorage.getItem("userToken"));
+  
+      if (userToken) {
+        const userData = jwtDecode(userToken.token);
+        setUserName(userData.name);
+      } else {
+        navigate("/login");
+      }
+    }, []);
+  
+    return (
+      <Layout>
+        <Header title="Perfil" userName={userName} />
+      </Layout>
+    );
+  }
---
+++ src/mocks/searchMock.ts
@@ -0,0 +1,13 @@
+export const searchMock = [
+    {
+      nome: "Santo André",
+      id: 4703,
+      estado: "PB",
+    },
+    {
+      nome: "Santo André",
+      id: 4704,
+      estado: "SP",
+    },
+  ];
+  
